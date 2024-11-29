f578618fddabeb17a043b738c5df1fce10c914e5
Eddy <eddy.chi.chu@gmail.com>
2024-11-29 03:17:36 -0800

    explicitly specify dtype

--- finetune_embedding_objective.py
+++ finetune_embedding_objective.py
@@ -22,7 +22,7 @@ from huggingface_hub import login
 # unsloth
 use_unsloth=False
 max_seq_length = 512 # Choose any! We auto support RoPE Scaling internally!
-dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
+dtype = torch.float16 # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
 load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
 
 
