f27b5f844f66b3c9682f5502b4f61b720b8c7f7e
Eddy <eddy.chi.chu@gmail.com>
2024-11-28 22:57:08 -0800

    fix prints

--- finetune_embedding_objective.py
+++ finetune_embedding_objective.py
@@ -294,7 +294,8 @@ def train(model, dataset, device, loss_fn, epochs=3, batch_size=4, lr=5e-5):
                     negative_hidden_states.append(outputs_negative.last_hidden_state[:, -1, :])  # Final hidden state of negative
             else:
                 outputs = model(input_ids=prompt_input_ids, attention_mask=prompt_attention_mask, output_hidden_states=True)
-                print(outputs.hidden_states.shape)
+                print(len(outputs.hidden_states))
+                print(outputs.hidden_states[-1])
                 prompt_hidden_state = outputs.hidden_states[-1][:, -1, :]  # Final hidden state of prompt
 
                 outputs_positive = model(input_ids=positive_input_ids, attention_mask=positive_attention_mask, output_hidden_states=True)
