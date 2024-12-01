c534da81070eb094e58d9a916bab8645ec762dd9
unknown <d0703887@gmail.com>
2024-12-01 00:23:27 -0800

    bang

--- daniel_finetune_embedding_objective.py
+++ daniel_finetune_embedding_objective.py
@@ -348,7 +348,7 @@ def train(model, dataset, device, loss_fn, epochs=3, batch_size=4, lr=5e-5, max_
 
                     outputs_positive = model(input_ids=positive_input_ids, attention_mask=positive_attention_mask)
                     positive_hidden_state = outputs_positive.last_hidden_state[:, -1, :]  # Final hidden state of positive
-                    print(len(neg_input_id))
+                    print(len(negative_input_ids))
                     negative_hidden_states = []
                     for neg_input_id, neg_attention_mask in zip(negative_input_ids, negative_attention_mask):
                         outputs_negative = model(input_ids=neg_input_id, attention_mask=neg_attention_mask)
