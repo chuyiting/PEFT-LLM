c40bbd252a823638d8c912735bb10eeb93b46553
unknown <d0703887@gmail.com>
2024-11-30 18:49:24 -0800

    bang

--- daniel_finetune_embedding_objective.py
+++ daniel_finetune_embedding_objective.py
@@ -73,7 +73,7 @@ def get_model(model_name, device, use_lora=True):
         model.to(torch.float16)
         return model, tokenizer
 
-    model = AutoModel.from_pretrained(model_name, trust_remote_code=True)
+    model = AutoModel.from_pretrained(model_name, trust_remote_code=True, load_in_4bit=True)
     tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 
     if use_lora:
