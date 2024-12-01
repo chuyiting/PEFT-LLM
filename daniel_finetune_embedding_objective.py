041f8ca2ea5bc141ed23dbfb6ddef90a4028a37f
unknown <d0703887@gmail.com>
2024-11-30 16:05:46 -0800

    bang

--- daniel_finetune_embedding_objective.py
+++ daniel_finetune_embedding_objective.py
@@ -345,9 +345,10 @@ def train(model, dataset, device, loss_fn, epochs=3, batch_size=4, lr=5e-5, max_
                 else:
                     # with autocast(device_type='cuda'):
                     print(prompt_input_ids.shape)
+                    print(prompt_input_ids)
                     outputs = model(input_ids=prompt_input_ids, attention_mask=prompt_attention_mask,
                                     output_hidden_states=True)
-                    print(outputs)
+                    print(outputs.hidden_states.shape)
                     prompt_hidden_state = outputs.hidden_states[-1][:, -1,
                                           :]  # Final hidden state of the last token of prompt
 
