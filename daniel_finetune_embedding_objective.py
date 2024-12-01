75d1a19c228ef3f864497f64220eae85b75c8836
unknown <d0703887@gmail.com>
2024-12-01 00:27:45 -0800

    bang

--- daniel_finetune_embedding_objective.py
+++ daniel_finetune_embedding_objective.py
@@ -337,7 +337,7 @@ def train(model, dataset, device, loss_fn, epochs=3, batch_size=4, lr=5e-5, max_
                 prompt_attention_mask = batch['prompt_attention_mask'].to(device)
                 positive_input_ids = batch['positive_input_ids'].to(device)
                 positive_attention_mask = batch['positive_attention_mask'].to(device)
-                print(negative_input_ids.shape)
+                print(batch['negative_input_ids'].shape)
                 negative_input_ids = [neg.to(device) for neg in batch['negative_input_ids']]
                 negative_attention_mask = [neg.to(device) for neg in batch['negative_attention_mask']]
 
