1153753fdd522b493606e8544fdaa914f559a135
Eddy <eddy.chi.chu@gmail.com>
2024-12-03 12:26:50 -0800

    add prints

--- validate_embedding_objective.py
+++ validate_embedding_objective.py
@@ -296,6 +296,7 @@ def evaluate(model, tokenizer, misconception_map, dataset, batch_size=16, cluste
             labels = batch['label']
              # Convert labels to the correct shape
             labels = torch.tensor([label[0] for label in labels])  # Extract the label from the list and convert
+            print(f'labels shape: {labels.shape}')
 
             # Get the model's output for the batch
             outputs = model(input_ids=prompt_input_ids, attention_mask=prompt_attention_mask, output_hidden_states=True)
@@ -321,6 +322,7 @@ def evaluate(model, tokenizer, misconception_map, dataset, batch_size=16, cluste
 
             # Ensure labels are a consistent shape
             correct_labels = torch.tensor(labels).unsqueeze(-1).cpu()  # Convert list to tensor and reshape to (B, 1)
+            print(f'correct labels shape: {correct_labels.shape}')
             # Append the correctly shaped labels
             all_correct_labels.append(correct_labels.numpy())
         
