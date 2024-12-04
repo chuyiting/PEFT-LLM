ed1289107bdb2a80133b53e592aa32becd27266b
keltonlee <keltonyt@gmail.com>
2024-12-03 19:21:21 -0800

    change for require grad

--- kelton_finetune_embedding_full.py
+++ kelton_finetune_embedding_full.py
@@ -146,15 +146,16 @@ class NewMisconceptionDataset(Dataset):
 
         misconception_inputs = tokenizer(list(self.misconception_map.values()), padding=True, truncation=True, return_tensors="pt").to(device)    
         with torch.no_grad():    
-            misconception_output = self.model(**misconception_inputs)
-        misconception_embeddings = mean_pooling(misconception_output, misconception_inputs['attention_mask'])
+            misconception_embeddings = mean_pooling(self.model(**misconception_inputs), misconception_inputs['attention_mask'])
         
         anchors = [" ".join([c, s, q, a, w]) for c, s, q, a, w in zip(
             self.construct_name, self.subject_name, self.question_text, self.correct_answer_text, self.wrong_answer_text)]
-        anchor_inputs = tokenizer(anchors, padding=True, truncation=True, return_tensors="pt").to(device)        
+        anchor_inputs = tokenizer(anchors, padding=True, truncation=True, return_tensors="pt").to(device)   
+        self.model.eval()     
         with torch.no_grad():    
-            anchor_output = self.model(**anchor_inputs)  
-        anchor_embeddings = mean_pooling(self.model(**anchor_output), anchor_inputs['attention_mask'])
+            anchor_embeddings = mean_pooling(self.model(**anchor_inputs), anchor_inputs['attention_mask'])
+            # anchor_output = self.model(**anchor_inputs)  
+        # anchor_embeddings = mean_pooling(self.model(**anchor_output), anchor_inputs['attention_mask'])
 
         similarity_matrix = cosine_similarity(anchor_embeddings, misconception_embeddings)
         top_k_indices = np.argsort(-similarity_matrix, axis=1)[:, :self.k]
